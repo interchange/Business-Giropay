c4921b8e21c335bf5a52a5dc5c45463eb2a9802e
Peter Mottram <peter@sysnix.com>
2016-04-05 11:29:20 +0200

    bump version and update Changes and README for release

--- lib/Business/Giropay.pm
+++ lib/Business/Giropay.pm
@@ -6,11 +6,11 @@ Business::Giropay - Giropay payments API
 
 =head1 VERSION
 
-Version 0.003
+Version 0.100
 
 =cut
 
-our $VERSION = '0.003';
+our $VERSION = '0.100';
 
 use Business::Giropay::Request::Bankstatus;
 use Business::Giropay::Request::Issuer;

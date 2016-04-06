e7a023e219399517974d5ae2a88627699723f4da
Peter Mottram <peter@sysnix.com>
2016-04-06 14:44:42 +0200

    add message attribute to Notification class

--- lib/Business/Giropay/Notification.pm
+++ lib/Business/Giropay/Notification.pm
@@ -13,6 +13,47 @@ use Moo;
 with 'Business::Giropay::Role::Core';
 use namespace::clean;
 
+# see http://api.girocheckout.de/en:girocheckout:resultcodes
+my %messages = (
+    4000 => 'transaction successful',
+
+    # giropay
+    4001 => 'bank offline',
+    4002 => 'online banking account invalid',
+    4500 => 'Zahlungsaugang unbekannt',
+
+    # Lastschrift
+    4051 => 'invalid bank account',
+
+    # Kreditkarte
+    4101 => 'issuing country invalid or unknown',
+    4102 => '3D-Secure or MasterCard SecureCode authorization failed',
+    4103 => 'validation date of card exceeded',
+    4104 => 'invalid or unknown card type',
+    4105 => 'limited-use card',
+    4106 => 'invalid pseudo-cardnumber',
+    4107 => 'card stolen, suspicious or marked to move in',
+
+    # PayPal
+    4151 => 'invalid PayPal token',
+    4152 => 'post-processing necessary at PayPal',
+    4153 => 'change PayPal payment method',
+    4154 => 'PayPal-payment is not completed',
+
+    # Allgemein
+    4501 => 'timeout / no user input',
+    4502 => 'user aborted',
+    4503 => 'duplicate transaction',
+    4504 => 'suspicion of manipulation or payment method temporarily blocked',
+    4505 => 'payment method blocked or rejected',
+    4900 => 'transaction rejected ',
+
+    # Age verification (giropay)
+    4020 => 'age verification successful',
+    4021 => 'age verification not possible',
+    4022 => 'age verification unsuccessful',
+);
+
 =head1 ATTRIBUTES
 
 =head2 reference
@@ -98,6 +139,18 @@ has resultPayment => (
     isa      => Int,
     required => 1,
     init_arg => 'gcResultPayment',
+);
+
+=head2 message
+
+The descriptive message for L</resultPayment>.
+
+=cut
+
+has message => (
+    is      => 'ro',
+    lazy    => 1,
+    default => sub { $messages{ $_[0]->resultPayment } || '' },
 );
 
 =head2 hash
